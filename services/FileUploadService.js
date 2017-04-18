556f75c7b5f3ae7c110e352ad496aedaf9b94a29
Dana Harris <optikool@yahoo.com>
2017-04-17 21:59:09 -0700

    Switched to using SuperAgent

--- services/FileUploadService.js
+++ services/FileUploadService.js
@@ -1,17 +1,15 @@
 'use strict';
 
 const express = require('express');
-const http = require('http');
-const FormData = require('form-data');
+const request = require('superagent');
 
 const router = express.Router();
 
 module.exports = class FileUploadService {
-    static uploadFile (req, res, next) {
+    static uploadFile(req, res, next) {
         let file = req.files['fupload'];
-        let form = new FormData();
 
-        if(!req.checkBody('file', 'Invalid image type.').isImage(file.mimetype)) {
+        if (!req.checkBody('file', 'Invalid image type.').isImage(file.mimetype)) {
             res.status(400).json({ data: { name: 'error', message: `Invalid Image Type: ${file.mimetype}!` } });
         } else {
             const options = {
@@ -19,46 +17,23 @@ module.exports = class FileUploadService {
                 port: 3010,
                 path: '/service/upload/',
                 method: 'POST',
-                headers: {
-                    'Content-Type': 'multipart/form-data; boundary=' + form.getBoundary()
-                }
             };
 
-            form.append("fupload",
-                file.data, { filename: file.name });
-
-            const request = http.request(options, (response) => {
-                console.log(`STATUS: ${response.statusCode}`);
-                console.log(`HEADERS: ${JSON.stringify(response.headers)}`);
-                let body = [];
-
-                response.setEncoding('utf8');
-
-                response.on('data', (chunk) => {
-                    console.log(`BODY: ${chunk}`);
-                    body.push(chunk);
+            request
+                .post(`http://${options.hostname}:${options.port}${options.path}`)
+                .attach('fupload', file.data, file.name)
+                .end(function (err, response) {
+                    if (err) {
+                        res.json({ data: { name: 'error', message: `problem with request: ${err.message}` } })
+                    } else {
+                        res.status(response.statusCode).json({
+                            data: {
+                                name: 'success',
+                                message: response.body.data.message
+                            }
+                        });
+                    }
                 });
-
-                response.on('end', () => {
-                    let d = JSON.parse(body.concat());
-
-                    res.status(response.statusCode).json({
-                        data: { 
-                            name: 'success', 
-                            message: d.data.message
-                        }
-                    });
-                });
-            });
-
-            form.pipe(request);
-
-            request.on('error', (e) => {
-                console.error(`problem with request: ${e}`);
-                res.json({ data: { name: 'error', message: `problem with request: ${e.message}` } })
-            });
-
-            request.end();
         }
     }
 }
